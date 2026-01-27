f8aae85909bc95e45a737f92cb545d32c2e96191
Fernando Bruno <fernandobrunofbsl@gmail.com>
2026-01-27 11:17:35 -0300

    Initial commit - ForumHub API

---
+++ src/main/java/forumhub/forumapi/ForumapiApplication.java
@@ -0,0 +1,13 @@
+package forumhub.forumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/forumhub/forumapi/ForumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package forumhub.forumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
